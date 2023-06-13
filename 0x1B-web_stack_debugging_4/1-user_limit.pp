95c832e522725e58b544483d1e6d58a9b89d6e73
munyasya21 <munyasyacynthia@gmail.com>
2023-06-13 13:31:01 -0700

    web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# fix limit
+exec { 'fix_limit_hbton_user':
+command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+path    => '/usr/local/bin/:/bin'
+}
+
+# Increase soft file limit hbton.
+exec { 'increase_soft_file':
+    command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+    path    => '/usr/local/bin/:/bin/'
+}
